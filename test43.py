3015823a4937de4c75d27166c57ac51e61656bab
tomek1421 <117310232+tomek1421@users.noreply.github.com>
2022-11-03 22:23:35 +0100

    Update test43.py

--- test43.py
+++ test43.py
@@ -1,4 +1,8 @@
-import pytest
-from plik import add
-def test_add():
-    assert add(5,3)==8
+import unittest
+from plik43 import add
+class TestStringMethods(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2,4),6)
+if __name__ == '__main__':
+    unittest.main()
